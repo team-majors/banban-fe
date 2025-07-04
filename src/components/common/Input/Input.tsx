e19e33e697135e9e5560fa8cca74e937aa79cd28
seolbotdae <seolbotdae@gmail.com>
2025-07-04 15:28:18 +0900

    chore: 포맷팅 수정

--- src/components/common/Input/Input.tsx
+++ src/components/common/Input/Input.tsx
@@ -1,16 +1,83 @@
 "use client";
 
-import React, { 
-  HTMLAttributes, 
-  InputHTMLAttributes, 
-  LabelHTMLAttributes, 
+import React, {
+  HTMLAttributes,
+  InputHTMLAttributes,
+  LabelHTMLAttributes,
   ReactNode,
   createContext,
   useContext,
-  useId
+  useId,
 } from "react";
-import styled, { css, CSSProperties } from "styled-components"
+import styled, { css, CSSProperties } from "styled-components";
 
+const InputContext = createContext<InputContextType | null>(null);
+
+export const Input = ({ $width, children, ...rest }: InputProps) => {
+  const baseId = useId();
+  const inputId = `${baseId}-field`;
+  const errorId = `${baseId}-error`;
+
+  return (
+    <InputContext.Provider value={{ inputId, errorId }}>
+      <InputWrapper $width={$width} {...rest}>
+        {children}
+      </InputWrapper>
+    </InputContext.Provider>
+  );
+};
+
+const Label = ({ children, ...rest }: LabelProps) => {
+  const context = useContext(InputContext);
+
+  return (
+    <InputLabelWrapper>
+      <StyledLabel {...rest} htmlFor={context?.inputId}>
+        {children}
+      </StyledLabel>
+    </InputLabelWrapper>
+  );
+};
+Label.displayName = "Input.Label";
+Input.Label = Label;
+
+const InputField = ({ $isValidate, $icon, ...rest }: InputFieldProps) => {
+  const context = useContext(InputContext);
+
+  const describeBy = !$isValidate ? context?.errorId : undefined;
+
+  return (
+    <InputFieldWrapper>
+      {!!$icon && <IconWrapper>{$icon}</IconWrapper>}
+      <StyledInputField
+        $isValidate={$isValidate}
+        $icon={$icon}
+        id={context?.inputId}
+        aria-invalid={!$isValidate}
+        aria-describedby={describeBy}
+        {...rest}
+      />
+    </InputFieldWrapper>
+  );
+};
+InputField.displayName = "Input.Field";
+Input.Field = InputField;
+
+const ErrorMessage = ({ children, ...rest }: ErrorMessageProps) => {
+  const context = useContext(InputContext);
+
+  return (
+    <ErrorMessageWrapper>
+      <StyledErrorMessage {...rest} id={context?.errorId}>
+        {children}
+      </StyledErrorMessage>
+    </ErrorMessageWrapper>
+  );
+};
+ErrorMessage.displayName = "Input.ErrorMessage";
+Input.ErrorMessage = ErrorMessage;
+
+// interface
 interface InputContextType {
   inputId: string;
   errorId: string;
@@ -27,12 +94,12 @@ interface InputFieldProps extends InputHTMLAttributes<HTMLInputElement> {
 }
 
 interface InputProps extends HTMLAttributes<HTMLDivElement> {
-  $width?: CSSProperties['width'];
-  children: ReactNode
+  $width?: CSSProperties["width"];
+  children: ReactNode;
 }
 
 interface InputWrapperProps {
-  $width?: CSSProperties['width'];
+  $width?: CSSProperties["width"];
 }
 
 interface LabelProps extends LabelHTMLAttributes<HTMLLabelElement> {
@@ -43,133 +110,70 @@ interface ErrorMessageProps extends HTMLAttributes<HTMLSpanElement> {
   children: ReactNode;
 }
 
-const InputContext = createContext<InputContextType | null>(null);
-
-export const Input = ({ $width, children, ...rest }: InputProps) => {
-  const baseId = useId();
-  const inputId = `${baseId}-field`;
-  const errorId = `${baseId}-error`;
-
-  return (
-    <InputContext.Provider value={{ inputId, errorId }}>
-      <InputWrapper $width={$width} {...rest}>{children}</InputWrapper>
-    </InputContext.Provider>
-  )
-}
-
-const Label = ({ children, ...rest }: LabelProps) => {
-  const context = useContext(InputContext);
-    
-  return (
-    <InputLabelWrapper>
-      <StyledLabel {...rest} htmlFor={context?.inputId}>
-        {children}
-      </StyledLabel>
-    </InputLabelWrapper>
-  )
-}
-Label.displayName = "Input.Label";
-Input.Label = Label;
-
-const InputField = ({ $isValidate, $icon, ...rest }: InputFieldProps) => {
-  const context = useContext(InputContext);
-
-  const describeBy = !$isValidate ? context?.errorId : undefined;
-
-  return (
-    <InputFieldWrapper>
-      {!!$icon && <IconWrapper>{$icon}</IconWrapper>}
-      <StyledInputField 
-        $isValidate={$isValidate}
-        $icon={$icon}
-        id={context?.inputId}
-        aria-invalid={!$isValidate}
-        aria-describedby={describeBy}
-        {...rest}
-      />
-    </InputFieldWrapper>
-  )
-}
-InputField.displayName = "Input.Field";
-Input.Field = InputField;
-
-const ErrorMessage = ({ children, ...rest }: ErrorMessageProps) => {
-  const context = useContext(InputContext);
-
-  return (
-    <ErrorMessageWrapper>
-      <StyledErrorMessage {...rest} id={context?.errorId}>
-        {children}
-      </StyledErrorMessage>
-    </ErrorMessageWrapper>
-  )
-}
-ErrorMessage.displayName = "Input.ErrorMessage";
-Input.ErrorMessage = ErrorMessage;
-
-
 // Wrapper 및 Styled 속성
 const InputWrapper = styled.div<InputWrapperProps>`
-  width: ${({$width}) => {
-    if ($width == undefined) return '100%';
-    return typeof $width === 'number' ? `${$width}px` : $width;
+  width: ${({ $width }) => {
+    if ($width == undefined) return "100%";
+    return typeof $width === "number" ? `${$width}px` : $width;
   }};
   display: flex;
   flex-direction: column;
-`
+`;
 
 const InputLabelWrapper = styled.div`
   padding-bottom: 6px;
-`
+`;
 
 const StyledLabel = styled.label`
   font-weight: bold;
   font-size: 14px;
-`
+`;
 
 const InputFieldWrapper = styled.div`
   position: relative;
-`
+`;
 
 const IconWrapper = styled.div`
   position: absolute;
   top: 50%;
   left: 12px;
   transform: translateY(-50%);
-`
+`;
 
 const StyledInputField = styled.input<StyledInputFieldProps>`
   width: 100%;
-  border: 1px solid #D5D7DA;
+  border: 1px solid #d5d7da;
   border-radius: 8px;
   padding: 10px 14px;
   box-shadow: 0 1px 2px rgba(10, 13, 18, 0.05);
   box-sizing: border-box;
 
-  ${({$isValidate}) => {
+  ${({ $isValidate }) => {
     if (!$isValidate) {
       return css`
-        border-color: #FF474F;
-        color: #FF474F;
+        border-color: #ff474f;
+        color: #ff474f;
 
         &:focus {
           color: black;
         }
-      `
+      `;
     }
   }}
 
-  ${({$icon}) => !!$icon && css`
-    padding-left: 35px;
-  `}
-`
+  ${({ $icon }) =>
+    !!$icon &&
+    css`
+      padding-left: 35px;
+    `}
+`;
 
 const ErrorMessageWrapper = styled.div`
   padding-top: 2px;
   padding-left: 5px;
-`
+`;
 
 const StyledErrorMessage = styled.span`
-  color: #FF474F;
+  color: #ff474f;
   font-size: 12px;
-`
+`;

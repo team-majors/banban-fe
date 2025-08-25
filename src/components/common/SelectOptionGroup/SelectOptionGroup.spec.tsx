f0db4a73685935b1232d796246185dc2edf36622
CEJSH <eun2763@naver.com>
2025-08-25 17:46:34 +0900

    chore: tsconfig.test.json 생성 및 기존 tsconfig 설정 최초 설정으로 변경

--- src/components/common/SelectOptionGroup/SelectOptionGroup.spec.tsx
+++ src/components/common/SelectOptionGroup/SelectOptionGroup.spec.tsx
@@ -1,29 +1,26 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-import userEvent from '@testing-library/user-event';
-import { SelectOptionGroup } from './SelectOptionGroup';
-import { describe, it, expect, vi } from 'vitest';
+import React from "react";
+import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+import { SelectOptionGroup } from "./SelectOptionGroup";
+import { describe, it, expect, vi } from "vitest";
 
-describe('SelectOptionGroup', () => {
-  const FIRST = '첫번째 옵션';
-  const SECOND = '두번째 옵션';
+describe("SelectOptionGroup", () => {
+  const FIRST = "첫번째 옵션";
+  const SECOND = "두번째 옵션";
 
-  it('옵션이 클릭 될 때 값이 밖으로 전달되는가?', async () => {
-    const onChange = vi.fn((state) => state);
+  it("옵션이 클릭 될 때 값이 밖으로 전달되는가?", async () => {
+    const onClick = vi.fn((state) => state);
     render(
       <SelectOptionGroup
         firstOptionString={FIRST}
         secondOptionString={SECOND}
-        onChange={onChange}
+        onClick={onClick}
         width="200px"
         height="100px"
         rowGap="8px"
-      />
+        selected={"none"}
+      />,
     );
-
-    // 마운트 시 none 전달
-    expect(onChange).toHaveBeenCalledTimes(1);
-    expect(onChange).toHaveBeenLastCalledWith('none');
 
     // 버튼 렌더링 확인
     const firstBtn = screen.getByText(FIRST);
@@ -33,10 +30,10 @@ describe('SelectOptionGroup', () => {
 
     // 제대로 된 값으로 호출되는지 확인
     await userEvent.click(firstBtn);
-    expect(onChange).toHaveBeenLastCalledWith('firstOption');
+    expect(onClick).toHaveBeenLastCalledWith("firstOption");
 
     // 제대로 된 값으로 호출되는지 확인
     await userEvent.click(secondBtn);
-    expect(onChange).toHaveBeenLastCalledWith('secondOption');
+    expect(onClick).toHaveBeenLastCalledWith("secondOption");
   });
 });

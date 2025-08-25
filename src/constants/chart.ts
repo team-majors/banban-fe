5247ccac4c924dee21cca2247cb4f274c3448e4f
CEJSH <eun2763@naver.com>
2025-08-25 17:33:32 +0900

    chore: 차트 관련 상수 추가

---
+++ src/constants/chart.ts
@@ -0,0 +1,6 @@
+export const CHART_CONFIG = {
+  dimensions: { width: 248, height: 248 },
+  outerRadius: 108,
+  angles: { start: 90, end: -270 },
+  animation: { duration: 1000, strokeDelay: 1200 },
+} as const;
